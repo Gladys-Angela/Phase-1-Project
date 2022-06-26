8d25b9a7018ea5207be163d7487f893f77f9131b
Gladys-Angela <gladysangelakamau@gmail.com>
2022-06-26 17:15:11 +0300

    steps

---
+++ index.js
@@ -0,0 +1,6 @@
+const baseURL= "http://www.omdbapi.com/?i=tt3896198&apikey=4038c2f4"
+const searchInput = document.getElementById("search-input");
+const movieTitleElement = document.getElementById("movie-title")
+const releasedElement = document.getElementById("released")
+const actorsElement = document.getElementById("actors")
+const plotElement = document.getElementById("plot")
