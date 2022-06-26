5f3edefca3f2fad5d7e4527a049611b9c647e53c
Gladys-Angela <gladysangelakamau@gmail.com>
2022-06-26 21:59:45 +0300

    btnstep

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ const movieTitleElement = document.getElementById("movie-title")
 const releasedElement = document.getElementById("released")
 const actorsElement = document.getElementById("actors")
 const plotElement = document.getElementById("plot")
+const searchBtn = document.getElementById('search-btn')
+
+searchBtn.addEventListener('click', (event) => {
+    const searchQuery = searchInput.value;
+    console.log(searchQuery)
+    searchByTitle(searchQuery)
+})
