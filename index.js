f26adaa728af6c7edf160d90715f8d1cab7e3b60
Gladys-Angela <gladysangelakamau@gmail.com>
2022-06-26 22:58:10 +0300

    saveButton

--- index.js
+++ index.js
@@ -4,10 +4,17 @@ const movieTitleElement = document.getElementById("movie-title")
 const releasedElement = document.getElementById("released")
 const actorsElement = document.getElementById("actors")
 const plotElement = document.getElementById("plot")
-const searchBtn = document.getElementById('search-btn')
+const searchBtn = document.getElementById("search-btn")
+const saveLaterBtn= document.getElementById("save-later-btn")
 
 searchBtn.addEventListener('click', (event) => {
     const searchQuery = searchInput.value;
     console.log(searchQuery)
     searchByTitle(searchQuery)
+})
+
+saveLaterBtn.addEventListener('click', (event) => {
+    if(currentResult) {
+        renderSavedMovie(currentResult)
+    }
 })
